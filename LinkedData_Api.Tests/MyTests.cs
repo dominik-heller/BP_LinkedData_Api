5b6b975867ed212ba9e7556f1990931af9ca2721
Dominik Heller <dominikheller@volny.cz>
2021-03-03 01:14:41 +0100

    Set up LinkedData_Api.Tests project. Before trying to perform controller separation.

--- LinkedData_Api.Tests/MyTests.cs
+++ LinkedData_Api.Tests/MyTests.cs
@@ -1,7 +1,41 @@
-﻿namespace LinkedData_Api.Tests
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using LinkedData_Api.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace LinkedData_Api.Tests
 {
     public class MyTests
     {
-        //DOPLNIT TESTY
+        private readonly HttpClient _client;
+
+        public MyTests()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            _client = appFactory.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("dbpedia")]
+        [InlineData("virtuoso")]
+        public async Task TestGraphs_ShouldReturnOk(string endpoint)
+        {
+            var response = await _client.GetAsync(ApiRoutes.EndpointGraphs.Replace("{endpoint}", endpoint));
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+        
+        [Theory]
+        [InlineData("_dbpedia")]
+        [InlineData("virtuoso1")]
+        public async Task TestGraphs_ShouldReturnNotFound(string endpoint)
+        {
+            var response = await _client.GetAsync(ApiRoutes.EndpointGraphs.Replace("{endpoint}", endpoint));
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+        
+        //TODO: add more tests....
     }
 }
